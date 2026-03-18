552601cd4949d829d20224f48fb1be07f0939c5a
Liesel <lyw8@cornell.edu>
2026-03-18 16:47:23 -0400

    add method around data loading code and add comment

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -2,19 +2,21 @@ import pandas as pd
 import re
 from pathlib import Path
 
+def load_movies(): 
 # create dataframe from json
-df = pd.read_json("dataset/tmdb.json")
+  df = pd.read_json("dataset/tmdb.json")
 
-def tokenize(text):
-  if pd.isna(text):
-    return []
-  text = text.lower()
-  return re.findall(r"\b[a-z0-9]+\b", text)
+  def tokenize(text):
+    if pd.isna(text):
+      return []
+    text = text.lower()
+    return re.findall(r"\b[a-z0-9]+\b", text)
 
-df["name_tokens"] = df["name"].apply(tokenize)
-df["overview_tokens"] = df["overview"].apply(tokenize)
+  df["name_tokens"] = df["name"].apply(tokenize)
+  df["overview_tokens"] = df["overview"].apply(tokenize)
 
-# both name and overview tokens
-df["all_tokens"] = df["name_tokens"] + df["overview_tokens"]
+  # both name and overview tokens
+  df["all_tokens"] = df["name_tokens"] + df["overview_tokens"]
 
-print(df[["id", "name", "name_tokens", "overview_tokens", "all_tokens"]])
+  print(df[["id", "name", "name_tokens", "overview_tokens", "all_tokens"]])
+  return df
